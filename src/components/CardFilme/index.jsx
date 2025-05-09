ea431369cc8668d230e5e13935a5851ab9502ca9
Mikelemag <mikelepinto2017@gmail.com>
2025-05-09 11:57:58 -0300

    pagina-filmes

--- src/components/CardFilme/index.jsx
+++ src/components/CardFilme/index.jsx
@@ -1,19 +1,25 @@
-export default function CardFilme(){
+export default function CardFilme({ filme }){
     return(
-        <div className="w-[350px] h-[280px] flex flex-col rounded-lg bg-[#222222]">
-            <div className="w-full h-[70%] rounded-t-lg">
+        <div className="w-[350px] h-[280px] flex flex-col rounded-lg bg-[#222222] 
+        border border-transparent hover:border-[#8f7bb8] 
+        cursor-pointer transition-all duration-300 ease-in-out">
+            <div className="w-full h-[70%] rounded-t-lg relative">
                 <img
                 className="w-full h-full rounded-t-lg object-cover"
-                src="https://m.media-amazon.com/images/S/pv-target-images/dcec5ce7e595f0b85c41b763a9f614c1f3cedfa9f64a7b884e15fc480930987b.jpg"
-                alt=""
+                src={filme.banner}
+                alt={filme.titulo}
                 />
+                <div className="absolute top-1 left-2 flex text-[#9ca3af]">
+                    <p className="text-[17px] text-green-400 font-semibold">{filme.nota}</p>
+                    <p>/10</p>
+                </div>
             </div>
             <div className="w-full h-[30%] flex flex-col pt-2 px-4">
-                <p className="text-[15px] font-bold">Love, Rosie</p>
-                <p className="text-[15px] text-[#8a898c]">Christian Ditter</p>
+                <p className="text-[15px] font-bold">{filme.titulo}</p>
+                <p className="text-[15px] text-[#8a898c]">{filme.diretor}</p>
                 <div className="w ful flex justify-between">
-                    <p className="text-[15px] text-[#8a898c]">2015</p>
-                    <div className="px-2 py-0 rounded-lg bg-[#8f7bd8]/10">Comédia Romântica</div>
+                    <p className="text-[15px] text-[#8a898c]">{filme.ano}</p>
+                    <div className="px-2 py-0 rounded-lg bg-[#8f7bd8]/10">{filme.genero}</div>
                 </div>
             </div>
         </div>
